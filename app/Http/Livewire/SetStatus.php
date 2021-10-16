02428a1bb907f481f8b9375960d985c8bb16ac79
Dejan <38137502+Dejan1990@users.noreply.github.com>
2021-10-16 15:31:51 +0200

    Set Status Notify All Voters

--- app/Http/Livewire/SetStatus.php
+++ app/Http/Livewire/SetStatus.php
@@ -5,11 +5,14 @@ namespace App\Http\Livewire;
 use App\Models\Idea;
 use Livewire\Component;
 use Illuminate\Http\Response;
+use App\Mail\IdeaStatusUpdatedMailable;
+use Illuminate\Support\Facades\Mail;
 
 class SetStatus extends Component
 {
     public $idea;
     public $status;
+    public $notifyAllVoters;
 
     public function mount(Idea $idea)
     {
@@ -26,7 +29,23 @@ class SetStatus extends Component
         $this->idea->status_id = $this->status;
         $this->idea->save();
 
+        if ($this->notifyAllVoters) {
+            $this->notifyAllVoters();
+        }
+
         $this->emit('statusWasUpdated');
+    }
+
+    public function notifyAllVoters()
+    {
+        $this->idea->votes()
+            ->select('name', 'email')
+            ->chunk(100, function ($voters) { // chunk koristimo u sl. da imamo mnogo glasova odjednom, to bi izazvalo problem (usporilo app) zato ogranicavamo br (100)
+                foreach ($voters as $user) {
+                    Mail::to($user)
+                        ->queue(new IdeaStatusUpdatedMailable($this->idea));
+                }
+            });
     }
     
     public function render()
